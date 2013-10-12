5fb0389365124fa43c7b3fb696d1fa01221ba0b8
EC2 Default User <ec2-user@ip-10-184-37-215.ec2.internal>
2013-10-12 03:56:35 +0000

    implemented login and lobby

---
+++ src/code.py
@@ -0,0 +1,60 @@
+import web
+import json
+
+urls = ('/', 'index',
+        '/login_action', 'login_attempt',
+        '/lobby', 'lobby',
+        '/creategame', 'game'
+        )
+render = web.template.render('templates/')
+
+
+users = dict()
+games = []
+
+class index:
+    def GET(self):
+        username = web.cookies().get('username')
+        if username:
+            location, gamename = users[username]
+            if location == 'lobby':
+                return render.lobby(username + location)
+            elif location == 'game':
+                return render.game()
+        else:
+            return render.login()
+
+class lobby:
+    def GET(self):
+        username = web.cookies().get('username')
+        if username:
+            return render.lobby(username)
+        else:
+            return render.login()
+
+class game:
+    def GET(self):
+        gamename = web.input()
+        games.append(gamename)
+        if username:
+            return render.lobby(username)
+        else:
+            return render.login()
+
+class login_attempt:
+    def GET(self):
+        user_data = web.input()
+        web.setcookie('username', user_data.user, 3600)
+        users[user_data.user] = ('lobby', '')
+        raise web.seeother('/')
+
+class req:
+    def GET(self):
+        contents = dict()
+        contents["day"] = 17
+        contents["year"] = 2005
+        return json.dumps(contents)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
